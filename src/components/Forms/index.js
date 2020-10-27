7df1ba43cdd7511216c2c0b3310bc790a6d2d871
Esmael-01 <Esmael@users.noreply.github.com>
2020-10-27 12:33:56 -0100

    first commit

---
+++ src/components/Forms/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import {
+  Container,
+  Header,
+  Panel,
+  Column,
+  Document,
+  Section,
+} from './styles';
+
+
+const Forms = () => {
+  return(
+    <Container>
+      <Header>
+        <h1>CONSERVATÓRIO DOS REGISTOS COMERCIAL E DE BENS MÓVEIS</h1>
+        <div>
+          <h2>Pedido de registo de compra e venda</h2>
+        </div>
+      </Header>
+      <Panel> 
+        <Column>
+          <Document>
+            <div>
+              
+            </div>
+          </Document>
+
+        </Column>
+
+        <Column>
+
+          <FormSection />
+          <FormSection />
+          <FormSection />
+
+        </Column>
+      </Panel>
+    </Container>
+  );
+};
+
+const FormSection = () => (
+  <Section>
+    <h4>Preencha o formulário</h4>
+
+    <div>
+      <span>
+        <p className="title">Conservatório dos registos comercial e de bens móveis</p>
+        <p className="description">
+          Pedido de registo de empresário comercial -Pessoa Singular- 
+        </p>
+      </span>
+      <span>
+        <p className="title">Conservatório dos registos comercial e de bens móveis</p>
+        <p className="description">Pedido de registo de compra e venda</p>
+      </span>
+    </div>
+
+    <a href="#">Saiba mais</a>
+  </Section>
+)
+
+export default Forms;
